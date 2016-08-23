b1075b125c976d6e6f09a3a0799220a6d128b751
Leandro Ezequiel Baldi <lbaldi@openpyme.com.ar>
2016-08-23 18:58:07 -0300

    [ADD] New project . Pic + Led + Keyboard

---
+++ PIC_LED_KEYBOARD/build/default/production/_ext/1807342946/unmc_lcd_216.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/_ext/1807342946/unmc_lcd_216.d  \
+ build/default/production/_ext/1807342946/unmc_lcd_216.p1:  \
+ C:/Archivos\ de\ programa/Unimic/unmc_01/sources/unmc_lcd_216.c  \
+C:/Archivos\ de\ programa/Unimic/unmc_01/headers/unmc_lcd_216.h 

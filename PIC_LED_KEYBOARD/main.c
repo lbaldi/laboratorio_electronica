3b03641c5ad295f5c32fdb4d86cdd8ec12b64d2f
Leandro Ezequiel Baldi <lbaldi@openpyme.com.ar>
2016-09-06 00:31:40 -0300

    no message

--- PIC_LED_KEYBOARD/main.c
+++ PIC_LED_KEYBOARD/main.c
@@ -1,8 +1,8 @@
-/*********************************************************************
+/*******************************************************************************
  *
  *                Unimic 01 Version 1.1   10 September 2015
  *
- ***********************************************************************
+ *******************************************************************************
  * FileName:        main.c
  * Dependencies:
  * Processor:       PIC18F26J50
@@ -11,13 +11,13 @@
  *
  * Software License Agreement
  *
- * THIS SOFTWARE IS PROVIDED IN AN �AS IS� CONDITION. NO WARRANTIES,
- * WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
- * TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
- * IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
- * CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- ***********************************************************************/
+ * THIS SOFTWARE IS PROVIDED IN AN �AS IS� CONDITION. NO WARRANTIES, WHETHER
+ * EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO 
+ * THIS SOFTWARE. THE COMPANY SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR 
+ * SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+ ******************************************************************************/
+
 #include <xc.h>
 #include<delays.h>
 #include<stdio.h>
@@ -27,15 +27,9 @@
 #include <unmc_inout_02.h>
 
 
-//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
-/// Funcion Caratula
-/// Display presentation day hour
-/// variable lecture diasem, anio, dia, hora, etc
-//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
-
-    void caratula(void)
-    {
-        lcd_comand(0b00001100);             //Enciende display sin cursor y sin blink  
+void Home(void){
+    
+        lcd_comand(0b00001100);        //Enciende display sin cursor y sin blink  
         lcd_gotoxy(1,1);        
         lcd_putrs("UNIMIC  TECLADO ");
         //sprintf(buffer2,"%02u/%02u/%02u",dia,mes,anio);
@@ -45,16 +39,10 @@
         lcd_gotoxy(1,2);
         //lcd_putrs(buffer2);
      
-    }
+}
 
-//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
-/// Funcion Setup
-///
-///
-//[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
-
-    void Setup(void)
-    {
+void Setup(void){
+    
     OSCTUNEbits.INTSRC=1;       //setea el oscilador de 32768 para el RTC
     OSCTUNEbits.PLLEN=0;        //desactiva PLL
     OSCCONbits.IRCF0=1;         //selecciona el clock en 8MHz
@@ -69,7 +57,7 @@
     //TRISBbits.TRISB0=0;
     //TRISCbits.TRISC0=0;
     ANCON0=0b11111111;          // Config AN7 to AN0 Digital Ports
-    //ANCON1=0b10010111;          // Config AN11 Analog Port
+    //ANCON1=0b10010111;        // Config AN11 Analog Port
     ANCON1=0b11111111;          // Config AN12 to AN8 Digital Ports
     ADCON0=0b00101101;          // Control AN11 Analog Port
     ADCON1=0b00010000;          // Config Analog Port
@@ -81,22 +69,10 @@
     lcd_comand(0b00001100);     //Display=on / Cursor=off / Blink=off
     LED_2_On;
     LED_3_On;
-    }
-/*------------------------------------------------------------------------------
-********************************************************************************
-Funcion main
-Funcion principal del programa
-********************************************************************************
---------------------------------------------------------------------------------*/
-int main(void)
-{
-Setup();
-caratula();
-while(1)
-    {
-    //Read_RTC();
-    //caratula();
-    //lcd_gotoxy(1,2);
+}
+
+void Write_Keyboard(void){
+    
     row1=1;row2=0;row3=0;row4=0;
     {
         if (column1==1){key=1;sprintf(buffer2,"%01u",key);lcd_putrs(buffer2);while(column1==1){};}
@@ -104,6 +80,7 @@ while(1)
         if (column3==1){key=3;sprintf(buffer2,"%01u",key);lcd_putrs(buffer2);while(column3==1){};}
         if (column4==1){key=11;lcd_putrs("A");while(column4==1){};}
     }
+
     row1=0;row2=1;row3=0;row4=0;
     {
         if (column1==1){key=4;sprintf(buffer2,"%01u",key);lcd_putrs(buffer2);while(column1==1){};}
@@ -111,6 +88,7 @@ while(1)
         if (column3==1){key=6;sprintf(buffer2,"%01u",key);lcd_putrs(buffer2);while(column3==1){};}
         if (column4==1){key=12;lcd_putrs("B");while(column4==1){};}
     }
+
     row1=0;row2=0;row3=1;row4=0;
     {
         if (column1==1){key=7;sprintf(buffer2,"%01u",key);lcd_putrs(buffer2);while(column1==1){};}
@@ -118,6 +96,7 @@ while(1)
         if (column3==1){key=9;sprintf(buffer2,"%01u",key);lcd_putrs(buffer2);while(column3==1){};}
         if (column4==1){key=13;lcd_putrs("C");while(column4==1){};}
     }
+
     row1=0;row2=0;row3=0;row4=1;
     {
         if (column1==1){key=14;lcd_putrs("*");while(column1==1){};}
@@ -125,8 +104,23 @@ while(1)
         if (column3==1){key=15;lcd_putrs("#");while(column3==1){};}
         if (column4==1){key=16;lcd_putrs("D");while(column4==1){};}
     }
+
     __delay_ms(98);             // 98ms retardo maximo para esta funcion
 
+}
+
+int main(void){
+    
+    Setup();
+    
+    Home();
+    
+    while(1){
+        
+        Write_Keyboard();
+    
     }
-return 0;
+    
+    return 0;
+
 }

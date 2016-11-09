0971238560f6508b64bbc1659c5f5c9d8915e0af
Leandro Ezequiel Baldi <lbaldi@openpyme.com.ar>
2016-11-09 16:50:29 -0300

    [ADD] Ultimo proyecto y cierre.

--- PIC_LED_DISPLAY/main.c
+++ PIC_LED_DISPLAY/main.c
@@ -6,6 +6,15 @@
 #include <unmc_config_01.h>
 #include <unmc_inout_01.h>
 
+char led_1_intensity[2];
+char led_2_intensity[2];
+char led_3_intensity[2];
+int intensity_1;
+int intensity_2;
+int intensity_3;
+int current_led;
+
+
 void pic_setup(void){
     
     OSCTUNEbits.INTSRC=1;       //setea el oscilador de 32768 para el RTC
@@ -29,30 +38,28 @@ void pic_setup(void){
     LED_3_Off;
 }
 
-void display_setup(void){
-    
-    lcd_init();
-    lcd_comand(0b00001100);         //Display=on / Cursor=off / Blink=off
-    //lcd_comand(0b00001110);       //Display=on / Cursor=on  / Blink=off
-    //lcd_comand(0b00001111);       //Display=on / Cursor=on  / Blink=on
-
-}
-
 void lcd_write(int column, int row, const char* string){
     
     lcd_gotoxy(column, row);
     lcd_putrs(string);
     
 }
-
 void display_init(void){
+    
+    lcd_init();
+    lcd_comand(0b00001100);         //Display=on / Cursor=off / Blink=off
+    
+}
+
+void display_home(void){
     
     lcd_write(1, 1, "| L1 | L2 | L3 |");
     lcd_write(1, 2, "|    |    |    |");
-
+    
 }
 
-void intensity_led(int led, int current, int off){
+void intensity_led(int led, int current, int off){  
+    
     switch(led){
         case 1:
             if(current == 0){
@@ -81,25 +88,72 @@ void intensity_led(int led, int current, int off){
     }
 }
 
+void leds_row_on(int led_1, int led_2, int led_3){
+    
+    for(int i = 0; i < 24; i++){
+        
+        lcd_gotoxy(16,2);        
+        __delay_ms(0.5);
+        
+        intensity_led(1, i, led_1);
+        intensity_led(2, i, led_2);
+        intensity_led(3, i, led_3);
+        
+    }  
+    
+}
+
+void switch_control(void){
+    
+    if(switch2 == 1){
+        while(switch2 == 1){};
+        intensity_2++;
+    }
+    
+    if(switch1 == 0){
+        while(switch1 == 0){};
+        switch(current_led){
+            case 1:
+                intensity_1++;
+                break;
+            case 2:
+                intensity_2++;
+                break;
+            case 3:
+                intensity_3++;
+                break;
+        }
+        
+    }
+  
+}
+
 int main(void){
     
-    pic_setup();
+    pic_setup();    
+    display_init();
+    display_home();
     
-    display_setup();
-        
+    intensity_1 = 1;
+    intensity_2 = 8;
+    intensity_3 = 15;
+    current_led = 1;
+    
     while(1){
+
+        switch_control();
         
-        display_init();
+        leds_row_on(intensity_1,intensity_2,intensity_3);
         
-        for(int i = 0; i < 32; i++){
-            
-            __delay_ms(0.5);
-            
-            intensity_led(1, i, 31);
-            intensity_led(2, i, 31);
-            intensity_led(3, i, 31);
-            
-        }
-  
+        sprintf(led_1_intensity, "%d", intensity_1);
+        lcd_write(3, 2, led_1_intensity);        
+        
+        sprintf(led_2_intensity, "%d", intensity_2);
+        lcd_write(8, 2, led_2_intensity);        
+        
+        sprintf(led_3_intensity, "%d", intensity_3);
+        lcd_write(13, 2, led_3_intensity);      
+        
     }  
+    
 }
